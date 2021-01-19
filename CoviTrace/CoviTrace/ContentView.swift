5b09d6e48d30426a382d29a2a74bc4a2a1a16347
Appe Latte <51999459+appe-latte@users.noreply.github.com>
2021-01-19 02:53:16 +0000

    ContentView creation

--- CoviTrace/CoviTrace/ContentView.swift
+++ CoviTrace/CoviTrace/ContentView.swift
@@ -13,13 +13,56 @@ struct ContentView: View {
     
     var body: some View {
         
-        
-        Text("Hello World!")
-        
-        // MARK: BACKGROUND COLOUR CODE:
-        // Color(red: 83 / 255, green: 82 / 255, blue: 116 / 255)
+        ZStack
+        {
+            // MARK: BACKGROUND COLOUR CODE:
+            Color(red: 83 / 255, green: 82 / 255, blue: 116 / 255)
+                .ignoresSafeArea()
+            
+            VStack(alignment:.center){
+                Image("wave")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 110)
+                Text("CoviTrace App")
+                    .foregroundColor(.white)
+                    .bold()
+                    .font(.largeTitle)
+                Text("Covid-19 Track & Trace")
+                    .foregroundColor(.white)
+            }
+            
+            VStack(alignment:.trailing) {
+                Spacer()
+                HStack {
+                    //                    Spacer()
+                    Button(action: {
+                        print("begin onboarding")
+                    }) {
+                        HStack {
+                            Text("ENTER")
+                                .fontWeight(.bold)
+                            Image(systemName: "chevron.right")
+                                .font(.title3)
+                        }
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color(red: 46 / 255, green: 153 / 255, blue: 168 / 255))
+                        .cornerRadius(40)
+                        
+                        
+                        // MARK: Long Button
+                        
+                        //                        .frame(minWidth: 0, maxWidth: 300)
+                        //                        .padding()
+                        //                        .foregroundColor(.white)
+                        //                        .background(Color(red: 46 / 255, green: 153 / 255, blue: 168 / 255))
+                        //                        .cornerRadius(40)
+                    }
+                }
+            }
+        }
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
