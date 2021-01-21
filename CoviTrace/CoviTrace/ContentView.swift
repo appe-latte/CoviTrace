f8c0233825a19ad0dcf4e3370b030b6a7c5170f7
Appe Latte <51999459+appe-latte@users.noreply.github.com>
2021-01-21 01:47:03 +0000

    Navigation
    - basic navigation from ContentView to MainView
    - some code tidying in AreaStatsView

--- CoviTrace/CoviTrace/ContentView.swift
+++ CoviTrace/CoviTrace/ContentView.swift
@@ -13,32 +13,31 @@ struct ContentView: View {
     
     var body: some View {
         
-        ZStack
-        {
-            // MARK: BACKGROUND COLOUR CODE:
-            Color(red: 83 / 255, green: 82 / 255, blue: 116 / 255)
-                .ignoresSafeArea()
-            
-            VStack(alignment:.center){
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 300, height: 300)
-            }
-            
-            VStack(alignment:.trailing) {
-                Spacer()
-                HStack {
-                    //                    Spacer()
-                    Button(action: {
-                        print("begin onboarding")
-                    }) {
-                        HStack {
+        NavigationView{
+            ZStack
+            {
+                // MARK: BACKGROUND COLOUR CODE:
+                Color(red: 83 / 255, green: 82 / 255, blue: 116 / 255)
+                    .ignoresSafeArea()
+                
+                VStack(alignment:.center){
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 300)
+                }
+                
+                VStack(alignment:.trailing) {
+                    Spacer()
+                    HStack {
+                        NavigationLink(
+                            destination: MainView()){
                             Text("ENTER")
                                 .fontWeight(.semibold)
                                 .font(.title3)
                             Image(systemName: "chevron.right")
                                 .font(.title3)
+                                
                             
                         }
                         .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250)
--- CoviTrace/CoviTrace/MainView.swift
+++ CoviTrace/CoviTrace/MainView.swift
@@ -11,123 +11,130 @@ struct MainView: View {
     
     var body: some View {
         
-        ZStack
-        {
-            // MARK: BACKGROUND COLOUR CODE:
-            Color(red: 83 / 255, green: 82 / 255, blue: 116 / 255)
-                .ignoresSafeArea()
+        NavigationView {
             
-            VStack(alignment:.center){
-                Image("safe-mark")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 250, height: 250)
-//                Spacer()
-                Text("You are currenly Covid-19:")
-                    .foregroundColor(.white)
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                Text("NEGATIVE")
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
-                    .scaledToFit()
-                Text("Last test results: 01.01.21")
-                    .foregroundColor(.white)
-//                Spacer()
-                Button(action: {
-                    print("show area statistics")
-                }) {
-                    HStack {
+            ZStack
+            {
+                // MARK: BACKGROUND COLOUR CODE:
+                Color(red: 83 / 255, green: 82 / 255, blue: 116 / 255)
+                    .ignoresSafeArea()
+                
+                // MARK: Shield & Status
+                VStack(alignment:.center){
+                    Image("safe-mark")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250, height: 250)
+                    Text("You are currenly Covid-19:")
+                        .foregroundColor(.white)
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    Text("NEGATIVE")
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .scaledToFit()
+                    Text("Last test results: 01.01.21")
+                        .foregroundColor(.white)
+                    
+                    // MARK: Area Statistics
+                    NavigationLink(
+                        destination: AreaStatsView()){
                         Image(systemName: "map.fill")
                             .font(.title3)
                         Text("Area Statistics")
                             .fontWeight(.semibold)
                             .font(.title3)
-                    }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250, alignment: .leading)
+                    }
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250, alignment: .leading)
                     .padding()
                     .foregroundColor(.white)
                     .background(Color(red: 46 / 255, green: 153 / 255, blue: 168 / 255))
-                    .cornerRadius(15)
-                }
-                Button(action: {
-                    print("symptom checker")
-                }) {
-                    HStack {
-                        Image(systemName: "stethoscope")
-                            .font(.title3)
-                        Text("Symptom Checker")
-                            .fontWeight(.semibold)
-                            .font(.title3)
-                    }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250, alignment: .leading)
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color(red: 46 / 255, green: 153 / 255, blue: 168 / 255))
-                    .cornerRadius(15)
-                }
-                Button(action: {
-                    print("show test results")
-                }) {
-                    HStack {
-                        Image(systemName: "folder.fill")
-                            .font(.title3)
-                        Text("Test Results")
-                            .fontWeight(.semibold)
-                            .font(.title3)
-                    }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250, alignment: .leading)
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color(red: 46 / 255, green: 153 / 255, blue: 168 / 255))
-                    .cornerRadius(15)
-                }
-                Button(action: {
-                    print("check-in start")
-                }) {
-                    HStack {
-                        Image(systemName: "scope")
-                            .font(.title3)
-                        Text("Check-in")
-                            .fontWeight(.semibold)
-                            .font(.title3)
-                    }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250, alignment: .leading)
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color(red: 46 / 255, green: 153 / 255, blue: 168 / 255))
-                    .cornerRadius(15)
-                }
-                Button(action: {
-                    print("show hygiene tips")
-                }) {
-                    HStack {
-                        Image(systemName: "info.circle.fill")
-                            .font(.title3)
-                        Text("Hygiene Tips")
-                            .fontWeight(.semibold)
-                            .font(.title3)
-                    }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250, alignment: .leading)
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color(red: 46 / 255, green: 153 / 255, blue: 168 / 255))
-                    .cornerRadius(15)
-                }
-                Button(action: {
-                    print("log exposure")
-                }) {
-                    HStack {
-                        Image(systemName: "bell.fill")
-                            .font(.title3)
-                        Text("EXPOSURE ALERT")
-                            .fontWeight(.semibold)
-                            .font(.title3)
-                    }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250)
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color(red: 255 / 255, green: 109 / 255, blue: 90 / 255))
                     .cornerRadius(15)
                     
-                }
-            }.padding()
-            .frame(minHeight: 0, maxHeight: 800 )
+                    // MARK: Symptom Checker
+                    Button(action: {
+                        print("symptom checker")
+                    }) {
+                        HStack {
+                            Image(systemName: "stethoscope")
+                                .font(.title3)
+                            Text("Symptom Checker")
+                                .fontWeight(.semibold)
+                                .font(.title3)
+                        }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250, alignment: .leading)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color(red: 46 / 255, green: 153 / 255, blue: 168 / 255))
+                        .cornerRadius(15)
+                    }
+                    
+                    // MARK: Test Results
+                    Button(action: {
+                        print("show test results")
+                    }) {
+                        HStack {
+                            Image(systemName: "folder.fill")
+                                .font(.title3)
+                            Text("Test Results")
+                                .fontWeight(.semibold)
+                                .font(.title3)
+                        }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250, alignment: .leading)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color(red: 46 / 255, green: 153 / 255, blue: 168 / 255))
+                        .cornerRadius(15)
+                    }
+                    Button(action: {
+                        print("check-in start")
+                    }) {
+                        HStack {
+                            Image(systemName: "scope")
+                                .font(.title3)
+                            Text("Check-in")
+                                .fontWeight(.semibold)
+                                .font(.title3)
+                        }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250, alignment: .leading)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color(red: 46 / 255, green: 153 / 255, blue: 168 / 255))
+                        .cornerRadius(15)
+                    }
+                    Button(action: {
+                        print("show hygiene tips")
+                    }) {
+                        HStack {
+                            Image(systemName: "info.circle.fill")
+                                .font(.title3)
+                            Text("Hygiene Tips")
+                                .fontWeight(.semibold)
+                                .font(.title3)
+                        }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250, alignment: .leading)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color(red: 46 / 255, green: 153 / 255, blue: 168 / 255))
+                        .cornerRadius(15)
+                    }
+                    Button(action: {
+                        print("log exposure")
+                    }) {
+                        HStack {
+                            Image(systemName: "bell.fill")
+                                .font(.title3)
+                            Text("EXPOSURE ALERT")
+                                .fontWeight(.semibold)
+                                .font(.title3)
+                        }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color(red: 255 / 255, green: 109 / 255, blue: 90 / 255))
+                        .cornerRadius(15)
+                        
+                    }
+                }.padding()
+                .frame(minHeight: 0, maxHeight: 800 )
+            }
+            
         }
     }
     
